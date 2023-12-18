e1544243a2f2d9d7b392e8359e04b6209cd13626
SMSaeedi <mahsa.saeedy@gmail.com>
2023-12-18 20:12:49 +0100

    Edit configuration Added test api "Hello world"

--- src/main/kotlin/com/linchpino/demo/LinchpinApp.kt
+++ src/main/kotlin/com/linchpino/demo/LinchpinApp.kt
@@ -2,10 +2,18 @@ package com.linchpino.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController("api")
 @SpringBootApplication
 class LinchpinApp
 
 fun main(args: Array<String>) {
-	runApplication<LinchpinApp>(*args)
+    runApplication<LinchpinApp>(*args)
+}
+
+@GetMapping("/test")
+fun helloKotlin(): String {
+    return "hello world"
 }
