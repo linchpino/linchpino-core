4ebcc460317efd8275e8d0d55a36e3413ee4ff85
mehdi.tilab <mehdi.tilab@virtudev.net>
2023-12-19 07:57:29 +0100

    Update Banner Upgrade to Java 21 Add spring-boot-starter-web

--- src/main/kotlin/com/linchpino/demo/LinchpinApp.kt
+++ src/main/kotlin/com/linchpino/demo/LinchpinApp.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class LinchpinApp
+open class LinchpinApp
 
 fun main(args: Array<String>) {
     runApplication<LinchpinApp>(*args)
