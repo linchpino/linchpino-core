bdb1393d48d936a312ec7ad4366d9e13fad436c5
Mohamad <sharifyy@gmail.com>
2024-03-17 18:21:58 +0330

    add spring security for password encryption

---
+++ src/main/kotlin/com/linchpino/core/security/SecurityConfig.kt
@@ -0,0 +1,25 @@
+package com.linchpino.core.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+
+	@Bean
+	fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+		return http
+			.csrf { it.disable() }
+			.cors { it.disable() } // todo configure cors
+			.authorizeHttpRequests { it.anyRequest().permitAll() }
+			.build()
+	}
+
+	@Bean
+	fun passwordEncoder():PasswordEncoder = BCryptPasswordEncoder()
+
+}
