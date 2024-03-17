8ecd714272c526b0171f3ba1e539bc5b8dbce479
Mohamad <sharifyy@gmail.com>
2024-03-17 10:09:38 +0330

    swagger annotation and documentation for create account api

--- src/main/kotlin/com/linchpino/core/controller/AccountController.kt
+++ src/main/kotlin/com/linchpino/core/controller/AccountController.kt
@@ -2,9 +2,12 @@ package com.linchpino.core.controller
 
 import com.linchpino.core.dto.AccountDto
 import com.linchpino.core.dto.CreateAccountResult
-import com.linchpino.core.exception.message.Message
 import com.linchpino.core.service.impl.AccountService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -16,8 +19,13 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("api/accounts")
 class AccountController(private val accountService: AccountService) {
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
+	@Operation(summary = "Create a new account")
+	@ResponseStatus(HttpStatus.CREATED)
+	@ApiResponses(value = [
+		ApiResponse(responseCode = "201", description = "Account created successfully"),
+		ApiResponse(responseCode = "400", description = "Invalid request body")
+	])
+	@PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun newAccount(@RequestBody accountDto: AccountDto): ResponseEntity<CreateAccountResult> {
         val result = accountService.newAccount(accountDto)
         return ResponseEntity.status(HttpStatus.CREATED).body(result)
