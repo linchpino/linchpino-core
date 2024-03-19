b0a64db3b56385ffc34f87d8b720d4d99a96be34
Mohamad <sharifyy@gmail.com>
2024-03-19 16:32:31 +0330

    many to many relation

--- src/main/kotlin/com/linchpino/core/entity/InterviewType.kt
+++ src/main/kotlin/com/linchpino/core/entity/InterviewType.kt
@@ -2,21 +2,16 @@ package com.linchpino.core.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.ManyToMany
 import jakarta.persistence.Table
-import lombok.AllArgsConstructor
-import lombok.Builder
-import lombok.Getter
-import lombok.NoArgsConstructor
-import lombok.Setter
+
 
 @Entity
 @Table(name = "INTERVIEW_TYPE")
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
 class InterviewType : AbstractEntity(){
     @Column(name = "NAME")
     lateinit var name: String
+
+	@ManyToMany(mappedBy = "interviewTypes")
+	val jobPositions = mutableListOf<JobPosition>()
 }
