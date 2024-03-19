787d7470d754c8452404328b98d38916aa21cbb8
Mohamad <sharifyy@gmail.com>
2024-03-19 08:20:04 +0330

    optimize imports

--- src/main/kotlin/com/linchpino/core/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/linchpino/core/exception/GlobalExceptionHandler.kt
@@ -2,7 +2,6 @@ package com.linchpino.core.exception
 
 import jakarta.servlet.http.HttpServletRequest
 import org.slf4j.LoggerFactory
-import org.springframework.context.MessageSource
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.FieldError
