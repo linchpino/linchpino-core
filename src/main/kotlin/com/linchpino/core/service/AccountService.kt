8f045ebd2cd801c9c3b744bd9d196a4823e6228a
Mohamad <sharifyy@gmail.com>
2024-04-07 12:32:04 +0330

    timezone functionality for searching timeslots (#15)
    Co-authored-by: Mohammad Sharifi <mohammad.sharifi@rashasoft.ir>

--- src/main/kotlin/com/linchpino/core/service/AccountService.kt
+++ src/main/kotlin/com/linchpino/core/service/AccountService.kt
@@ -2,6 +2,7 @@ package com.linchpino.core.service
 
 import com.linchpino.core.dto.CreateAccountRequest
 import com.linchpino.core.dto.CreateAccountResult
+import com.linchpino.core.dto.MentorWithClosestTimeSlot
 import com.linchpino.core.dto.mapper.AccountMapper
 import com.linchpino.core.entity.Account
 import com.linchpino.core.repository.AccountRepository
@@ -9,6 +10,7 @@ import lombok.extern.slf4j.Slf4j
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.ZoneOffset
 import java.time.ZonedDateTime
 
 @Service
@@ -29,6 +31,9 @@ class AccountService(
     }
 
 
-    fun findMentorsWithClosestTimeSlotsBy(date: ZonedDateTime, interviewTypeId: Long) =
-        repository.closestMentorTimeSlots(date, interviewTypeId)
+    fun findMentorsWithClosestTimeSlotsBy(date: ZonedDateTime, interviewTypeId: Long): List<MentorWithClosestTimeSlot> {
+        val from = date.withZoneSameInstant(ZoneOffset.UTC)
+        val to = from.plusHours(24)
+        return repository.closestMentorTimeSlots(from,to, interviewTypeId)
+    }
 }
