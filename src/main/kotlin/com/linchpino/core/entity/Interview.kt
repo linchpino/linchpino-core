a05ce154c14d59e371655d6347597ac4f901a8b9
SMSaeedi <mahsa.saeedy@gmail.com>
2024-04-10 20:54:41 +0200

    24-add-interview-request-api

--- src/main/kotlin/com/linchpino/core/entity/Interview.kt
+++ src/main/kotlin/com/linchpino/core/entity/Interview.kt
@@ -5,38 +5,27 @@ import jakarta.persistence.FetchType
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
-import lombok.AllArgsConstructor
-import lombok.Builder
-import lombok.Getter
-import lombok.NoArgsConstructor
-import lombok.Setter
 
 @Entity
 @Table(name = "INTERVIEW")
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
 class Interview : AbstractEntity() {
     @JoinColumn(name = "JOB_POSITION_ID", referencedColumnName = "ID", nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
-    private val jobPosition: JobPosition? = null
+    var jobPosition: JobPosition? = null
 
     @JoinColumn(name = "INTERVIEW_TYPE_ID", referencedColumnName = "ID", nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
-    private val interviewType: InterviewType? = null
+    var interviewType: InterviewType? = null
 
     @JoinColumn(name = "TIME_SLOT_ID", referencedColumnName = "ID", nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
-    private val timeSlot: MentorTimeSlot? = null
+    var timeSlot: MentorTimeSlot? = null
 
-    @JoinColumn(name = "MENTOR_ACCOUNT_ID", referencedColumnName = "ID", nullable = false)
+    @JoinColumn(name = "MENTOR_ACCOUNT_ID", referencedColumnName = "ID", nullable = true)
     @ManyToOne(fetch = FetchType.LAZY)
-    private val mentorAccount: Account? = null
+    var mentorAccount: Account? = null
 
     @JoinColumn(name = "JOB_SEEKER_ACCOUNT_ID", referencedColumnName = "ID", nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
-    private val jobSeekerAccount: Account? = null
-
+    var jobSeekerAccount: Account? = null
 }
---
+++ src/main/kotlin/com/linchpino/core/service/InterviewService.kt
@@ -0,0 +1,73 @@
+package com.linchpino.core.service
+
+import com.linchpino.core.dto.CreateInterviewRequest
+import com.linchpino.core.dto.CreateInterviewResult
+import com.linchpino.core.entity.Account
+import com.linchpino.core.entity.Interview
+import com.linchpino.core.enums.AccountStatusEnum
+import com.linchpino.core.enums.AccountTypeEnum
+import com.linchpino.core.repository.AccountRepository
+import com.linchpino.core.repository.InterviewRepository
+import com.linchpino.core.repository.InterviewTypeRepository
+import com.linchpino.core.repository.JobPositionRepository
+import com.linchpino.core.repository.MentorTimeSlotRepository
+import lombok.extern.slf4j.Slf4j
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Slf4j
+@Transactional
+class InterviewService(
+    private val interviewRepository: InterviewRepository,
+    private val accountRepository: AccountRepository,
+    private val jobPositionRepository: JobPositionRepository,
+    private val interviewTypeRepository: InterviewTypeRepository,
+    private val mentorTimeSlotRepository: MentorTimeSlotRepository,
+) {
+
+    fun createInterview(request: CreateInterviewRequest): CreateInterviewResult {
+        val jobSeekerAccount = isJobSeekerHasAccount(request.jobSeekerEmail)
+        val interview = populateInterviewObject(request, jobSeekerAccount)
+        interviewRepository.save(interview)
+        return interviewResult(interview)
+    }
+
+    private fun isJobSeekerHasAccount(jobSeekerEmail: String): Account? {
+        return accountRepository.findByEmail(jobSeekerEmail) ?: createSilentAccForJobSeeker(jobSeekerEmail)
+    }
+
+    private fun createSilentAccForJobSeeker(email: String): Account {
+        return accountRepository.save(Account().apply {
+            this.email = email
+            type = AccountTypeEnum.JOB_SEEKER
+            status = AccountStatusEnum.DEACTIVATED
+        })
+    }
+
+    fun populateInterviewObject(createInterviewRequest: CreateInterviewRequest, jobSeekerAcc: Account?): Interview {
+        var position = jobPositionRepository.getReferenceById(createInterviewRequest.jobPositionId)
+        var mentorAcc = accountRepository.getReferenceById(createInterviewRequest.mentorAccId)
+        var typeInterview = interviewTypeRepository.getReferenceById(createInterviewRequest.interviewTypeId)
+        var mentorTimeSlot = mentorTimeSlotRepository.getReferenceById(createInterviewRequest.timeSlotId)
+
+        return Interview().apply {
+            jobPosition = position
+            interviewType = typeInterview
+            timeSlot = mentorTimeSlot
+            mentorAccount = mentorAcc
+            jobSeekerAccount = jobSeekerAcc
+        }
+    }
+
+    private fun interviewResult(entity: Interview): CreateInterviewResult {
+        return CreateInterviewResult(
+            entity.id,
+            entity.jobPosition?.id,
+            entity.interviewType?.id,
+            entity.timeSlot?.id,
+            entity.mentorAccount?.id,
+            entity.jobSeekerAccount?.email,
+        )
+    }
+}
