565299e83ff9c26c39b24102feaa8889e2f91c46
Mohamad <sharifyy@gmail.com>
2024-05-12 11:24:33 +0330

    Drop type column from account table.

--- src/main/resources/db/migration/V11__2024_05_11_1602.sql
+++ src/main/resources/db/migration/V11__2024_05_11_1602.sql
@@ -2,3 +2,5 @@ update role set title = 'GUEST' where id = 1;
 update role set title = 'JOB_SEEKER' where id = 2;
 update role set title = 'MENTOR' where id = 3;
 update role set title = 'ADMIN' where id = 4;
+
+alter table account drop column type;
