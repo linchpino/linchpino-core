23343737fdd303399c448f1b29232558019d90b3
Mohamad <sharifyy@gmail.com>
2024-05-12 11:17:53 +0330

    Fix AccountControllerTest package

--- src/test/kotlin/com/linchpino/core/controller/AccountControllerTest.kt
+++ src/test/kotlin/com/linchpino/core/controller/AccountControllerTest.kt
@@ -1,7 +1,6 @@
-package com.linchpino.core.controller.account
+package com.linchpino.core.controller
 
 import com.linchpino.core.captureNonNullable
-import com.linchpino.core.controller.AccountController
 import com.linchpino.core.dto.*
 import com.linchpino.core.enums.AccountStatusEnum
 import com.linchpino.core.enums.AccountTypeEnum
