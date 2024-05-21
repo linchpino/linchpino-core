16cde00ca7b9b7b0b4c3f2165e3c6ea99d965874
SMSaeedi <mahsa.saeedy@gmail.com>
2024-05-21 12:26:40 +0200

    sendEmail test

--- src/main/kotlin/com/linchpino/core/config/ThymeleafConfig.kt
+++ src/main/kotlin/com/linchpino/core/config/ThymeleafConfig.kt
@@ -1,43 +1,29 @@
 package com.linchpino.core.config
 
-import org.springframework.context.ApplicationContext
-import org.springframework.context.MessageSource
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import org.thymeleaf.spring6.SpringTemplateEngine
-import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver
+import org.thymeleaf.templatemode.TemplateMode
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
 import org.thymeleaf.templateresolver.ITemplateResolver
 
-
 @Configuration
-class ThymeleafConfig(
-    private val applicationContext: ApplicationContext
-) {
+class ThymeleafConfig {
     @Bean
     fun templateEngine(): SpringTemplateEngine? {
         val templateEngine = SpringTemplateEngine()
         templateEngine.setTemplateResolver(templateResolver())
         templateEngine.enableSpringELCompiler = true
-        templateEngine.setTemplateEngineMessageSource(messageSource())
         return templateEngine
     }
 
     private fun templateResolver(): ITemplateResolver? {
-        val resolver = SpringResourceTemplateResolver()
-        resolver.setApplicationContext(applicationContext)
-//        resolver.prefix = "/resources/templates/"
-//        resolver.suffix = ".html"
-//        resolver.templateMode = TemplateMode.HTML
-//        resolver.characterEncoding = "UTF-8"
+        val resolver = ClassLoaderTemplateResolver()
+        resolver.prefix = "templates/"
+        resolver.suffix = ".html"
+        resolver.templateMode = TemplateMode.HTML
+        resolver.characterEncoding = "UTF-8"
+        resolver.isCacheable = false
         return resolver
-    }
-
-    @Bean
-    fun messageSource(): MessageSource? {
-        val messageSource = ReloadableResourceBundleMessageSource()
-        messageSource.setBasename("classpath:application")
-        messageSource.setDefaultEncoding("UTF-8")
-        return messageSource
     }
 }
