86a01711634d48a57907e8a03144ad97bb5de50d
Mohammad Sharifi <mohammad.sharifi@rashasoft.ir>
2024-06-19 10:48:30 +0330

    Change meet credentials to json file

--- src/main/kotlin/com/linchpino/core/service/SpaceServiceProvider.kt
+++ src/main/kotlin/com/linchpino/core/service/SpaceServiceProvider.kt
@@ -4,30 +4,24 @@ import com.google.api.gax.core.FixedCredentialsProvider
 import com.google.apps.meet.v2.SpacesServiceClient
 import com.google.apps.meet.v2.SpacesServiceSettings
 import com.google.auth.oauth2.ServiceAccountCredentials
-import com.linchpino.core.config.MeetCredential
 import com.linchpino.core.exception.ErrorCode
 import com.linchpino.core.exception.LinchpinException
 import org.springframework.stereotype.Service
 import java.io.ByteArrayInputStream
 import java.io.IOException
+import org.springframework.beans.factory.annotation.Value
 
 @Service
-class SpaceServiceProvider(meetCredential: MeetCredential) {
-    private val json = """
-            {
-            "type": "service_account",
-            "private_key_id": "${meetCredential.privateKeyId}",
-            "private_key": "${meetCredential.privateKey}",
-            "client_id": "${meetCredential.clientId}",
-            "client_email": "${meetCredential.clientEmail}"
-            }
-        """.trimIndent()
+class SpaceServiceProvider(
+    @Value("\${meet.credential}") private val credentialPath: String
+) {
 
     fun spaceServiceClient(): SpacesServiceClient {
 
-        val credentials = ServiceAccountCredentials.fromStream(ByteArrayInputStream(json.toByteArray()))
-            .createScoped("https://www.googleapis.com/auth/meetings.space.created")
-            .createDelegated("linchpino@linchpino.com")
+        val credentials =
+            ServiceAccountCredentials.fromStream(this.javaClass::class.java.getResourceAsStream(credentialPath))
+                .createScoped("https://www.googleapis.com/auth/meetings.space.created")
+                .createDelegated("linchpino@linchpino.com")
 
         val settings = SpacesServiceSettings.newBuilder()
             .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
