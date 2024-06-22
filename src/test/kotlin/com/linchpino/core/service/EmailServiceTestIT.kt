d48211b1b43c56f198f758cc24f20ec2b5a48781
SMSaeedi <mahsa.saeedy@gmail.com>
2024-06-22 22:28:57 +0200

    add-invitation-file-to-interview-email-jobseeker

--- src/test/kotlin/com/linchpino/core/service/EmailServiceTestIT.kt
+++ src/test/kotlin/com/linchpino/core/service/EmailServiceTestIT.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
-import java.util.UUID
+import java.util.*
 
 @SpringBootTest
 @Import(PostgresContainerConfig::class)
