94a90e0dc5efcf4dbdf1638baf2988019f34ff72
Homa <mohammad.masoomihomayoun@theapsgroup.com>
2024-06-30 17:31:34 +0200

    127-get-arguments-to-generate-a-related-roadmap

--- src/test/kotlin/com/linchpino/ai/service/domain/PromptTest.java
+++ src/test/kotlin/com/linchpino/ai/service/domain/PromptTest.java
@@ -6,34 +6,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PromptTest {
 
-    private final String DEFAULT_ROADMAP_PROMPT = """
-        I want to have a roadmap to reach to level {Senior Data Scientist}.
-        Could you please give me a roadmap to reach level {Senior Data Scientist}
-        Please give me road map in json format like below
-        {
-            "targetLevel": {Senior Data Scientist},
-            "steps": [
-                {
-                    "step": "1",
-                    "description": "step 1 description",
-                    "subSteps": [
-                        {
-                            "step": "1.1",
-                            "description": "step 1.1 description"
-                        },
-                        {
-                            "step": "1.2",
-                            "description": "step 1.2 description"
-                        }
-                    ]
-                }
-            ]
-        }
-        Provide me response in json without any other information.
-        """;
-
     @Test
     void getDefaultRoadmapPrompt() {
+        String DEFAULT_ROADMAP_PROMPT = """
+            I want to have a roadmap to reach to level {Senior Data Scientist}.
+            Could you please give me a roadmap to reach level {Senior Data Scientist}
+            Please give me road map in json format like below
+            {
+                "targetLevel": {Senior Data Scientist},
+                "steps": [
+                    {
+                        "step": "1",
+                        "description": "step 1 description",
+                        "subSteps": [
+                            {
+                                "subStep": "1.1",
+                                "description": "step 1.1 description"
+                            },
+                            {
+                                "subStep": "1.2",
+                                "description": "step 1.2 description"
+                            }
+                        ]
+                    }
+                ]
+            }
+            Provide me response in json without any other information.
+            """;
         assertEquals(DEFAULT_ROADMAP_PROMPT, Prompt.of(RequestDetail.getDefaultRequestDetail()).toString());
     }
 }
