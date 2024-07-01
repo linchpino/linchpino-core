2470e4da183988962e5206a7352e3985286f0771
Mohammad (Homayoun) <8209897+Mohammad-Masoomi-Homayoun@users.noreply.github.com>
2024-07-01 17:36:07 +0200

    127 get arguments to generate a related roadmap (#85)
    * 127-get-arguments-to-generate-a-related-roadmap
    - Introduce AIService interface to unify accessing related AI service
    - Add isDefault to AIServiceName to set a service as default service
    - Introduce InteractionType to define the AI interaction like chat model or function call
    - Refactoring prompt class to easily handle the request detail as input
    - Add linkedinUrl attribute to the RequestDetail
    - Change the GET to POST to load more data from client side and introduce RoadmapRequestDTO to transfer client request data to service
    
    * 127-get-arguments-to-generate-a-related-roadmap
    - Introduce AIService interface to unify accessing related AI service
    - Add isDefault to AIServiceName to set a service as default service
    - Introduce InteractionType to define the AI interaction like chat model or function call
    - Refactoring prompt class to easily handle the request detail as input
    - Add linkedinUrl attribute to the RequestDetail
    - Change the GET to POST to load more data from client side and introduce RoadmapRequestDTO to transfer client request data to service
    
    * 127-get-arguments-to-generate-a-related-roadmap
    - Delete InteractionType and remove extra fields from RequestDetail
    - Introduce application property default-service-provider to select AI service provider
    - Apply effects of removing RequestDetail extra fields
    
    * 127-get-arguments-to-generate-a-related-roadmap
    
    ---------
    
    Co-authored-by: Homa <mohammad.masoomihomayoun@theapsgroup.com>

--- src/test/kotlin/com/linchpino/ai/service/domain/PromptTest.java
+++ src/test/kotlin/com/linchpino/ai/service/domain/PromptTest.java
@@ -1,79 +1,38 @@
 package com.linchpino.ai.service.domain;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PromptTest {
 
-    private final String DEFAULT_ROADMAP_PROMPT = """
-        Hi, I'm {John Doe} and
-        I am started my field which is {Data Science} and
-        based on estimation my level is {Junior Data Scientist} and
-        I want to have a roadmap to reach to level {Senior Data Scientist} and goal is {Data Scientist}
-        could you please give me a roadmap to reach goal {Data Scientist} and level {Senior Data Scientist}
-        please give me road map in json format like below
-        {
-            "currentLevel": {Junior Data Scientist},
-            "targetLevel": {Senior Data Scientist},
-            "goal": {Data Scientist},
-            "steps": [
-                {
-                    "step": 1,
-                    "description": "step 1 description",
-                    "subSteps": [
-                        {
-                            "step": 1.1,
-                            "description": "step 1.1 description"
-                        },
-                        {
-                            "step": 1.2,
-                            "description": "step 1.2 description"
-                        }
-                    ]
-                }
-            ]
-        }
-        provide me response in json without any other information
-        """;
-
-    private final String testPrompt = """
-        Hi, I'm {Test} and
-        I am started my field which is {Software tester} and
-        based on estimation my level is {Junior Test Engineer} and
-        I want to have a roadmap to reach to level {Senior Test Engineer} and goal is {Test engineer}
-        could you please give me a roadmap to reach goal {Test engineer} and level {Senior Test Engineer}
-        please give me road map in json format like below
-        {
-            "currentLevel": {Junior Test Engineer},
-            "targetLevel": {Senior Test Engineer},
-            "goal": {Test engineer},
-            "steps": [
-                {
-                    "step": 1,
-                    "description": "step 1 description",
-                    "subSteps": [
-                        {
-                            "step": 1.1,
-                            "description": "step 1.1 description"
-                        },
-                        {
-                            "step": 1.2,
-                            "description": "step 1.2 description"
-                        }
-                    ]
-                }
-            ]
-        }
-        provide me response in json without any other information
-        """;
-
-    @Test
-    void getRoadmapPrompt() {
-        assertEquals(testPrompt, Prompt.getRoadmapPrompt("Test", "Software tester", "Junior Test Engineer", "Senior Test Engineer", "Test engineer"));
-    }
-
     @Test
     void getDefaultRoadmapPrompt() {
-        assertEquals(DEFAULT_ROADMAP_PROMPT, Prompt.getDefaultRoadmapPrompt());
+        String DEFAULT_ROADMAP_PROMPT = """
+            I want to have a roadmap to reach to level {Senior Data Scientist}.
+            Could you please give me a roadmap to reach level {Senior Data Scientist}
+            Please give me road map in json format like below
+            {
+                "targetLevel": {Senior Data Scientist},
+                "steps": [
+                    {
+                        "step": "1",
+                        "description": "step 1 description",
+                        "subSteps": [
+                            {
+                                "subStep": "1.1",
+                                "description": "step 1.1 description"
+                            },
+                            {
+                                "subStep": "1.2",
+                                "description": "step 1.2 description"
+                            }
+                        ]
+                    }
+                ]
+            }
+            Provide me response in json without any other information.
+            """;
+        assertEquals(DEFAULT_ROADMAP_PROMPT, Prompt.of(RequestDetail.getDefaultRequestDetail()).toString());
     }
 }
