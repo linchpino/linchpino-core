ee5c74e11710a547646a97673288898bbd713784
Mohamad <sharifyy@gmail.com>
2024-07-10 14:21:48 +0330

    InterviewType API. (#97)

--- src/main/kotlin/com/linchpino/core/dto/InterviewTypeDTO.kt
+++ src/main/kotlin/com/linchpino/core/dto/InterviewTypeDTO.kt
@@ -1,3 +1,8 @@
 package com.linchpino.core.dto
 
+import jakarta.validation.constraints.NotBlank
+
 data class InterviewTypeSearchResponse(val id: Long, val title: String)
+
+
+data class InterviewTypeCreateRequest(@field:NotBlank val name:String, val jobPositionId:Long)
