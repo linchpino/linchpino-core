0600899ac0b68f6aa75dc7d36759cf2db62b40bc
Amin MirzaAhmadi <103266791+aminmirzaahmadi@users.noreply.github.com>
2024-07-16 12:41:58 +0330

    Update init.sql

--- initscripts/init.sql
+++ initscripts/init.sql
@@ -1,3 +1,4 @@
 CREATE USER linchpinodbuser;
+GRANT ALL ON SCHEMA public TO linchpinodbuser;
 CREATE DATABASE $currentbranches;
 GRANT ALL PRIVILEGES ON DATABASE $currentbranches TO linchpinodbuser;
