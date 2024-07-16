5b0833c98ef30f801d565dafd9a15eab9304d19a
mehdi.tilab <mehdi.tilab@virtudev.net>
2024-07-16 19:30:26 +0200

    116: Organize import

--- src/test/kotlin/com/linchpino/core/service/EmailServiceTestIT.kt
+++ src/test/kotlin/com/linchpino/core/service/EmailServiceTestIT.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
-import java.util.*
+import java.util.UUID
 
 @SpringBootTest
 @Import(PostgresContainerConfig::class)
