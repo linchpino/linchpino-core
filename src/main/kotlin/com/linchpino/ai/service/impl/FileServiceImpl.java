dfb8f48cba445323ef5385e9d00a6020e9dc867a
Homa <mohammad.masoomihomayoun@theapsgroup.com>
2024-08-25 11:20:51 +0200

    LinkedIn profile PDF parser in the service layer only - Refactor the directory traversal vulnerability

--- src/main/kotlin/com/linchpino/ai/service/impl/FileServiceImpl.java
+++ src/main/kotlin/com/linchpino/ai/service/impl/FileServiceImpl.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 @Service
 public class FileServiceImpl implements FileService {
@@ -17,13 +20,25 @@ public class FileServiceImpl implements FileService {
 
     @Override
     public File saveFile(MultipartFile file) {
-        String originalFilename = FilenameUtils.getName(file.getOriginalFilename());
-        File convFile = new File(fileUploadDir + File.separator + originalFilename);
-        try {
-            file.transferTo(convFile);
-        } catch (IOException e) {
-            throw new RuntimeException("Error in saving file: " + e.getMessage(), e);
+        if (file == null) {
+            throw new IllegalArgumentException("File is null");
         }
-        return convFile;
+        File localTempFile;
+        try {
+            String originalFilename = FilenameUtils.getName(file.getOriginalFilename());
+            InputStream initialStream = file.getInputStream();
+            byte[] buffer = new byte[initialStream.available()];
+            int readBytes = initialStream.read(buffer);
+            if (readBytes == -1) {
+                throw new IllegalArgumentException("Error in reading file");
+            }
+            localTempFile = new File(fileUploadDir + File.separator + originalFilename);
+            try (OutputStream outStream = new FileOutputStream(localTempFile)) {
+                outStream.write(buffer);
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Error in saving file: " + e.getMessage(), e);
+        }
+        return localTempFile;
     }
 }
