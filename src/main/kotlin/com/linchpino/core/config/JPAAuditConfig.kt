51de43ce4b3ff52a9f20489ea26b7edae5dfcf44
Mohammad Sharifi <mohammad.sharifi@rashasoft.ir>
2024-09-16 20:24:36 +0330

    fix cyclic method calling of repository

--- src/main/kotlin/com/linchpino/core/config/JPAAuditConfig.kt
+++ src/main/kotlin/com/linchpino/core/config/JPAAuditConfig.kt
@@ -1,17 +1,20 @@
 package com.linchpino.core.config
 
 import com.linchpino.core.repository.AccountRepository
+import jakarta.persistence.EntityManagerFactory
 import java.util.Optional
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.domain.AuditorAware
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import org.springframework.security.oauth2.server.resource.introspection.OAuth2IntrospectionAuthenticatedPrincipal
 
 @Configuration
-class JPAAuditConfig {
+@EnableJpaAuditing
+class JPAAuditConfig(private val entityManagerFactory: EntityManagerFactory) {
 
     @Bean
     fun auditorProvider(accountRepository: AccountRepository): AuditorAware<Long> {
@@ -23,11 +26,22 @@ class JPAAuditConfig {
                     else -> null
                 } ?: return@AuditorAware Optional.empty()
 
-                val id = accountRepository.findByEmailIgnoreCase(email)?.id
+                val id = findUserIdByEmail(email)
                 return@AuditorAware Optional.ofNullable<Long>(id)
 
             } ?: return@AuditorAware Optional.empty()
 
         }
     }
+
+    fun findUserIdByEmail(email: String): Long? {
+        val em = entityManagerFactory.createEntityManager()
+        em.transaction.begin()
+        val result = em.createQuery("select a.id from Account a where a.email = :email", Long::class.java)
+            .setParameter("email", email).resultList
+        em.transaction.commit()
+        em.close()
+        return if(result.isEmpty()) null else result[0]
+    }
 }
+
